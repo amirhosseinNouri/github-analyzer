e1af2d510c742d920cda2f45b4f6c45030b2ee1a
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 19:13:48 +0330

    Error page done

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,20 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>Sorry the page you trid cannot be found</h3>
+      <Link to="/" className="btn">Back home</Link>
+    </div>
+    
+  </Wrapper>;
 };
 const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
-  background: var(--clr-primary-10);
+
+  min-height : 100vh;
+  display : grid ;
+  place-items : center;
+  background-color : var(--clr-primary-10);
   text-align: center;
   h1 {
-    font-size: 10rem;
+    font-size : 10rem;
   }
-  h3 {
-    color: var(--clr-grey-3);
-    margin-bottom: 1.5rem;
+  h3{
+    color : var(--clr-grey-3);
+    margin-bottom : 1.5rem;
   }
+
 `;
 export default Error;
