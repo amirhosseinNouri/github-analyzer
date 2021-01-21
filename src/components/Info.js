fc512c47337b5b2c85c4b4c86f47bc7be7bab509
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 19:39:39 +0330

    Setup Provider of Context API

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { GoRepo, GoGist } from "react-icons/go";
+import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const UserInfo = () => {
+  const data = React.useContext(GithubContext);
+  console.log(data);
   return <h2>user info component</h2>;
 };
 
