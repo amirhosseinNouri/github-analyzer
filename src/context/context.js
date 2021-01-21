58d8abe1d5cc367aa29a222e6b82e1f2bd3cb43a
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 19:55:22 +0330

    state created using useState hook, in Context

--- src/context/context.js
+++ src/context/context.js
@@ -9,8 +9,15 @@ const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
+  // States
+  const [githubUser, setGithubUser] = React.useState(mockUser);
+  const [repos, setRepos] = React.useState(mockRepos);
+  const [followers, setFollowers] = React.useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value={"hello"}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
