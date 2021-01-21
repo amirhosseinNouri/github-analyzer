cea8742ca254d5d1918d8badc0b89071ba9d9a77
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-21 19:22:52 +0330

    Login page styled

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,22 +3,29 @@ import { useAuth0 } from '@auth0/auth0-react';
 import styled from 'styled-components';
 import loginImg from '../images/login-img.svg';
 const Login = () => {
-  return <h2>login page</h2>;
+  return <Wrapper>
+    <div className="container">
+      <img src={loginImg} alt="Github user"/>
+      <h1>Github user</h1>
+      <button className="btn">Login</button>
+    </div>
+  </Wrapper>;
 };
 const Wrapper = styled.section`
-  min-height: 100vh;
-  display: grid;
-  place-items: center;
+
+  min-height : 100vh;
+  display : grid ;
+  place-items : center; 
   .container {
-    width: 90vw;
-    max-width: 600px;
-    text-align: center;
+    width : 90vw;
+    max-width : 600px;
+    text-align: center ;
   }
   img {
-    margin-bottom: 2rem;
+    margin-bottom : 2rem;
   }
   h1 {
-    margin-bottom: 1.5rem;
+    margin-bottom : 1.5rem;
   }
 `;
 export default Login;
