2481bbb4c679bfc23414ab1440c029707bc97872
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-24 22:28:11 +0330

    Extract data for chatrs from Context repo object

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,27 +4,46 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
+  let languages = repos.reduce((total, current) => {
+    const { language } = current;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      // total[language] = {
+      //   ...total[language],
+      //   value: total[language].value + 1,
+      // };
+      total[language].value++;
+    }
+
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
 
   // STEP 2 - Chart Data
-const chartData = [
-  {
-    label: "HTML",
-    value: "13"
-  },
-  {
-    label: "Css",
-    value: "23"
-  },
-  {
-    label: "JavaScript",
-    value: "80"
-  },
-
-];
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "Css",
+      value: "23",
+    },
+    {
+      label: "JavaScript",
+      value: "80",
+    },
+  ];
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie3D data={chartData}></Pie3D>
+        <Pie3D data={languages}></Pie3D>
       </Wrapper>
     </section>
   );
