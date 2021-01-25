addef78d525f3741425b1dc5c35680fa3dced999
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-25 12:06:38 +0330

    Navbar component styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,23 +38,25 @@ const Navbar = () => {
 const Wrapper = styled.nav`
   padding: 1.5rem;
   margin-bottom: 4rem;
-  background: var(--clr-white);
+  background-color: var(--clr-white);
   text-align: center;
   display: grid;
   grid-template-columns: auto auto 100px;
   justify-content: center;
   align-items: center;
   gap: 1.5rem;
+
   h4 {
     margin-bottom: 0;
     font-weight: 400;
   }
   img {
     width: 35px !important;
-    height: 35px;
     border-radius: 50%;
     object-fit: cover;
+    height: 35px;
   }
+
   button {
     background: transparent;
     border: transparent;
@@ -63,6 +65,12 @@ const Wrapper = styled.nav`
     letter-spacing: var(--spacing);
     color: var(--clr-grey-5);
     cursor: pointer;
+    transition: var(--transition);
+    outline: none;
+
+    &:hover {
+      color: var(--clr-primary-5);
+    }
   }
 `;
 
