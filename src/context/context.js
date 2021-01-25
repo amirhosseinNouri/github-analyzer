15b641999a9afc15b603a9f6cf1fca6f9c677044
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-25 10:46:51 +0330

    All requests synced

--- src/context/context.js
+++ src/context/context.js
@@ -29,14 +29,20 @@ const GithubProvider = ({ children }) => {
     if (response) {
       setGithubUser(response.data);
       const { login, followers_url } = response.data;
-      //repos
-      axios
-        .get(`${rootUrl}/users/${login}/repos?per_page=100`)
-        .then((response) => setRepos(response.data));
-      //followers
-      axios
-        .get(`${followers_url}?per_page=100`)
-        .then((response) => setFollowers(response.data));
+
+      await Promise.allSettled([
+        axios.get(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios.get(`${followers_url}?per_page=100`),
+      ]).then((results) => {
+        const [repos, followers] = results;
+        const status = "fulfilled";
+        if (repos.status === status) {
+          setRepos(repos.value.data);
+        }
+        if (followers.status === status) {
+          setFollowers(followers.value.data);
+        }
+      });
     } else {
       toggleError(true, "There is no user with that usename");
     }
