70bd5009a4eba55c3b5a2bcf8ca4413ef6743110
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-25 11:13:16 +0330

    Auth0 initial setup done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { GithubProvider } from './context/context';
-import { Auth0Provider } from '@auth0/auth0-react';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
+// dev-6ol65p6j.us.auth0.com
+// Bf8H4Wo1n1Ie98mKOuYDNEoGFo9BxLBx
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-    <App />
-    </GithubProvider>
-    
+    <Auth0Provider
+      domain="dev-6ol65p6j.us.auth0.com"
+      clientId="Bf8H4Wo1n1Ie98mKOuYDNEoGFo9BxLBx"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
