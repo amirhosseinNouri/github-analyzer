d8d094f29f26d37fc9ebdb343eb86dd5e111b375
Amirhossein <amir.h.nouri2000@gmail.com>
2021-01-25 10:16:29 +0330

    Info gird layout editted

--- src/components/Info.js
+++ src/components/Info.js
@@ -62,7 +62,7 @@ const Item = ({ icon, label, value, color }) => {
 
 const Wrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   gap: 1rem 2rem;
 
   @media screen and (min-width: 640px) {
