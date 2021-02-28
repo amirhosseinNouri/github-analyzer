b9b3ec139ed8fb2813e19b6ca9f7d113fe15cc2f
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 18:08:56 +0330

    :x: Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 // dev-6ol65p6j.us.auth0.com
@@ -23,7 +23,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
