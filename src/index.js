9474f950ddcffee08825bf9d9c2bb369410b7727
Amirhossein <amir.h.nouri2000@gmail.com>
2021-02-28 18:18:59 +0330

    :hammer: Add service worker again

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 // dev-6ol65p6j.us.auth0.com
@@ -13,7 +13,7 @@ ReactDOM.render(
       domain="dev-6ol65p6j.us.auth0.com"
       clientId="Bf8H4Wo1n1Ie98mKOuYDNEoGFo9BxLBx"
       redirectUri={window.location.origin}
-      cacheLocation = 'localstorage'
+      cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
@@ -23,3 +23,7 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
